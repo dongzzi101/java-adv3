cb7b2cd124ea05b0c5393a727d021cb076f4d4a2
dongwook <dongzzi101@gmail.com>
2025-05-24 23:25:20 +0900

    [람다가 필요한 이유1] :
    프로그래밍에서 중복을 제거하고, 좋은 코드를 유지하는 핵심은 변하는 부분과 변하지 않는 부분을 분리하는 것

---
+++ src/lamda/start/Ex0RefMain.java
@@ -0,0 +1,17 @@
+package lamda.start;
+
+public class Ex0RefMain {
+
+    public static void main(String[] args) {
+        hello("java");
+        hello("spring");
+    }
+
+    public static void hello(String name) {
+        System.out.println("프로그램 시작");
+        System.out.println("Hello " + name);
+        System.out.println("프로그램 종료");
+    }
+
+
+}
