9ba8f6a96a4908b0678f26bdde09bb3072241015
dongwook <dongzzi101@gmail.com>
2025-05-25 11:58:11 +0900

    [람다가 필요한 이유3] :
    람다
    자바에서 메서드의 매개변수에 인수로 전달할 수 있는 것은 크게 2가지
    - int, double 같은 기본형 타입
    - 참조형 타입(인스턴스)

---
+++ src/lamda/start/Ex1RefMainV3.java
@@ -0,0 +1,38 @@
+package lamda.start;
+
+import lamda.Procedure;
+
+import java.util.Random;
+
+// 익명클래스 사용, 변수 제거, 익명 클래스의 매개변수 직접 전달
+public class Ex1RefMainV3 {
+
+    public static void hello(Procedure procedure) {
+        long startNs = System.nanoTime();
+        procedure.run();
+        long endNs = System.nanoTime();
+        System.out.println("실행 시간 : " + (endNs - startNs) + "ns");
+    }
+
+
+    public static void main(String[] args) {
+        hello(new Procedure() {
+            @Override
+            public void run() {
+                int randomValue = new Random().nextInt(6) + 1;
+                System.out.println("주사위 = " + randomValue);
+            }
+        });
+
+        hello(new Procedure() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= 3; i++) {
+                    System.out.println("i = " + i);
+                }
+            }
+        });
+    }
+
+
+}
---
+++ src/lamda/start/Ex1RefMainV4.java
@@ -0,0 +1,32 @@
+package lamda.start;
+
+import lamda.Procedure;
+
+import java.util.Random;
+
+// 익명클래스 사용, 변수 제거, 익명 클래스의 매개변수 직접 전달
+public class Ex1RefMainV4 {
+
+    public static void hello(Procedure procedure) {
+        long startNs = System.nanoTime();
+        procedure.run();
+        long endNs = System.nanoTime();
+        System.out.println("실행 시간 : " + (endNs - startNs) + "ns");
+    }
+
+
+    public static void main(String[] args) {
+        hello(() -> {
+            int randomValue = new Random().nextInt(6) + 1;
+            System.out.println("주사위 = " + randomValue);
+        });
+
+        hello(() -> {
+            for (int i = 1; i <= 3; i++) {
+                System.out.println("i = " + i);
+            }
+        });
+    }
+
+
+}
